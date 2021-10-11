195ac221021475dc53c1d4a2cbc2a7e53a3af1d0
Shadee Merhi <shadmerhi@gmail.com>
2021-10-10 22:17:57 -0600

    add get/post user methods

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -3,6 +3,7 @@ import React, { useState, useContext, useEffect } from 'react';
 import { getAuth, signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
 
 import '../firebase';
+import axios from 'axios';
 
 import { useDispatch } from 'react-redux';
 import { logUserIn, logUserOut } from '../actions/user';
@@ -17,17 +18,41 @@ const useAuth = () => {
   const [currentUser, setCurrentUser] = useState(null);
   const dispatch = useDispatch();
   const onGoogleSignIn = async () => {
-    const result = await signInWithPopup(
-      auth,
-      new GoogleAuthProvider(),
-    );
+    const result = await signInWithPopup(auth, new GoogleAuthProvider());
     const credential = GoogleAuthProvider.credentialFromResult(result);
     const token = credential.accessToken;
     const { user } = result;
-    console.log('HERE IS ALL STUFF', result, credential.providerId, token, user);
+    console.log(
+      'HERE IS ALL STUFF',
+      result,
+      credential.providerId,
+      token,
+      user,
+    );
   };
 
   const logout = () => auth.signOut();
+
+  const findUserByFirebaseUID = async (uid) => {
+    try {
+      const userAPIResponse = await axios.get(`/users/${uid}`);
+      const user = userAPIResponse.data;
+      return user;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleAuthenticatedUser = async (firebaseUserObject) => {
+    // Check if the user exists in the application database
+    const existingUser = await findUserByFirebaseUID(firebaseUserObject.uid);
+    if (existingUser) {
+      return dispatch(logUserIn(existingUser));
+    }
+    // Creating a new user
+    await axios.post('/users', firebaseUserObject);
+    return dispatch(logUserIn(firebaseUserObject));
+  };
 
   // Handles currentUser object state by subscribing to changes to said object
   useEffect(() => {
@@ -37,8 +62,7 @@ const useAuth = () => {
         dispatch(logUserOut());
         return;
       }
-      // Will need to process user to check if in application database
-      dispatch(logUserIn({ firebaseUserObject: user, username: '' }));
+      handleAuthenticatedUser(user);
     });
 
     return unsubscribe;
