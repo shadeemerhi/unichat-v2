a635741f0c90863308f066c91d095c00e2051fe7
Shadee Merhi <shadmerhi@gmail.com>
2021-10-11 16:29:35 -0600

    remove unused imports, remove default layout styling

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,13 @@
-import { useState, useEffect } from 'react';
-import { GetServerSideProps } from 'next';
-
-import type { NextPage } from 'next';
-import { useSelector, useDispatch } from 'react-redux';
-
-// import AuthProvider from '../src/hooks/useAuthProvider';
-import useAuth from '../src/hooks/useAuth';
+import { useSelector } from 'react-redux';
 
 import SignIn from './SignIn';
-import styles from '../styles/Home.module.css';
 import { AppState } from '../store';
 
 const Home = () => {
   const counter = useSelector((state: AppState) => state.counter);
 
   return (
-    <div className={styles.container}>
-      <p>UNICHAT</p>
+    <div>
       <p>{counter}</p>
       <SignIn />
     </div>
