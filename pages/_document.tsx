9544dc5aa186bd993539d624e9bd5b82870d5c42
Shadee Merhi <shadmerhi@gmail.com>
2021-10-11 16:27:24 -0600

    create document component, insert font

---
+++ pages/_document.tsx
@@ -0,0 +1,20 @@
+import Document, {
+  Html, Head, Main, NextScript,
+} from 'next/document';
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet" />
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
