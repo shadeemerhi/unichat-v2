a046ac68bc56bec798e3cf748377ea10227134c3
Shadee Merhi <shadmerhi@gmail.com>
2021-10-11 22:47:17 -0600

    inject server-side rendered styles

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,8 +1,22 @@
 import Document, {
-  Html, Head, Main, NextScript,
+  Html, Head, Main, NextScript, DocumentContext,
 } from 'next/document';
 
+import { ServerStyleSheets } from '@mui/styles';
+
 class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const sheets = new ServerStyleSheets();
+    const originalRenderPage = ctx.renderPage;
+
+    ctx.renderPage = () => originalRenderPage({
+      enhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
+    });
+    const initialProps = await Document.getInitialProps(ctx);
+
+    return initialProps;
+  }
+
   render() {
     return (
       <Html>
