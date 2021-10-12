91c7b992980feae3f776bbee9be987d47d7a7459
Shadee Merhi <shadmerhi@gmail.com>
2021-10-11 22:49:15 -0600

    move authModal to Layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,26 +1,10 @@
-import { useState } from 'react';
-
-// Redux
-import { useSelector } from 'react-redux';
-import { AppState } from '../store';
-
 // Components
 import SignIn from './SignIn';
-import TransitionModal from '../src/components/Nav/AuthModal';
 
-const Home = () => {
-  const counter = useSelector((state: AppState) => state.counter);
-
-  const [open, setOpen] = useState(false);
-  const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
-
-  return (
-    <div>
-      <TransitionModal open={open} handleOpen={handleOpen} handleClose={handleClose} />
-      <SignIn handleOpen={handleOpen} />
-    </div>
-  );
-};
+const Home = () => (
+  <div>
+    <SignIn />
+  </div>
+);
 
 export default Home;
