4791a0395a1828d1d3aefb34539cffaef7a1ea12
Shadee Merhi <shadmerhi@gmail.com>
2021-10-11 21:30:45 -0600

    add navbar styling

--- src/components/Nav/Navbar.tsx
+++ src/components/Nav/Navbar.tsx
@@ -1,11 +1,63 @@
 import React from 'react';
 
-import styles from './navbar.module.css';
+import { makeStyles, useTheme } from '@mui/styles';
+import { Theme } from '@mui/material';
 
-const Navbar: React.FC = () => (
-  <div className={styles.navbar}>
-    <button type="button">Login</button>
-  </div>
-);
+const useStyles = makeStyles((theme: Theme) => ({
+  root: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    height: '50px',
+    padding: '10px',
+  },
+
+  buttonContainer: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    width: '230px',
+  },
+
+  button: {
+    width: '100px',
+  },
+
+  loginButton: {
+    background: 'white',
+    border: `1px solid ${theme.palette.primary.main}`,
+    color: theme.palette.primary.main,
+    '&:hover': {
+      background: theme.palette.primary.light,
+    },
+  },
+
+  signupButton: {
+    background: theme.palette.primary.main,
+    color: 'white',
+    '&:hover': {
+      background: theme.palette.primary.dark,
+    },
+  },
+
+  logo: {
+    color: theme.palette.primary.main,
+    fontSize: '18pt',
+  },
+}));
+
+const Navbar = (): JSX.Element => {
+  const theme = useTheme();
+
+  const classes = useStyles(theme);
+  return (
+    <div className={classes.root}>
+      <p className={classes.logo}>UniChat</p>
+      <div className={classes.buttonContainer}>
+        <button type="button" className={`${classes.button} ${classes.loginButton}`}>Log In</button>
+        <button type="button" className={`${classes.button} ${classes.signupButton}`}>Sign Up</button>
+      </div>
+    </div>
+  );
+};
 
 export default Navbar;
