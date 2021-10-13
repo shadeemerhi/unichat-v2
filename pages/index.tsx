62ec8a6d1363cfeaca0b1331db7491bca5659a01
Shadee Merhi <shadmerhi@gmail.com>
2021-10-13 01:12:48 -0600

    test user state selector

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,18 @@
+import { useSelector } from 'react-redux';
+import useAuth from '../src/hooks/useAuth';
+import { AppState } from '../store';
+
 // Components
 import SignIn from './SignIn';
 
-const Home = () => (
-  <div>
-    <SignIn />
-  </div>
-);
+const Home = () => {
+  const userState = useSelector((state: AppState) => state.userState);
+  return (
+    <>
+      {userState.user ? <p>{`Welcome to UniChat, ${userState.user.email}`}</p> : <p>Login or Create an Account</p>}
+      <SignIn />
+    </>
+  );
+};
 
 export default Home;
