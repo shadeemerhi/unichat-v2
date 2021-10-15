5becc2f1d7aeec03b5a7e934139ef5f1a5f54ca4
Shadee Merhi <shadmerhi@gmail.com>
2021-10-14 21:14:31 -0600

    add base email/password login/signup logic

--- src/components/Nav/AuthModal/Login.tsx
+++ src/components/Nav/AuthModal/Login.tsx
@@ -1,10 +1,18 @@
 /* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
-import React, { useState } from 'react';
+import React, { useState, FormEvent } from 'react';
 
 // MUI
 import { makeStyles, useTheme } from '@mui/styles';
 import { Theme } from '@mui/material';
+
+// Redux
+import { useDispatch, useSelector } from 'react-redux';
+import { closeModal } from '../../../actions/authModal';
+import { AppState } from '../../../../store';
+
+// Authentication hook
+import useAuth from '../../../hooks/useAuth';
 
 const useStyles = makeStyles((theme: Theme) => ({
 
@@ -17,15 +25,50 @@ interface LoginProps {
 }
 
 const Login = ({ handleModalViewToggle, view, classes }: LoginProps): JSX.Element => {
+  // Authentication functions
+  const { login } = useAuth();
+
+  // Redux state
+  const userState = useSelector((state: AppState) => state.userState);
   // Local state
   const [email, setEmail] = useState<string>('');
   const [password, setPassword] = useState<string>('');
+
+  // Styling
   const theme = useTheme();
+
+  const onSubmit = async (event: FormEvent) => {
+    event.preventDefault();
+    if (!email || !password) return;
+    try {
+      await login(email, password);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
-    <form className={classes.formContainer}>
+    <form className={classes.formContainer} onSubmit={onSubmit}>
       <p className={`${classes.headerText} no_margin`}>Login</p>
-      <input type="email" placeholder="email" className={classes.formInput} value={email} onChange={(event: React.ChangeEvent<HTMLInputElement>) => setEmail(event.target.value)} />
-      <input type="password" placeholder="password" className={classes.formInput} value={password} onChange={(event: React.ChangeEvent<HTMLInputElement>) => setPassword(event.target.value)} />
+      <input
+        type="email"
+        placeholder="email"
+        className={classes.formInput}
+        value={email}
+        onChange={(event: React.ChangeEvent<HTMLInputElement>) => setEmail(event.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="password"
+        className={classes.formInput}
+        value={password}
+        onChange={(event: React.ChangeEvent<HTMLInputElement>) => setPassword(event.target.value)}
+      />
+      {/* Will change below to error container */}
+      <span className={classes.toggleViewContainer}>
+        {userState.error
+          && <p className={`${classes.toggleViewText} no_margin`}>{userState.error}</p>}
+      </span>
       <button type="submit" className={classes.submitButton}>Login</button>
       <span className={classes.toggleViewContainer}>
         Need an account?
--- src/components/Nav/AuthModal/Signup.tsx
+++ src/components/Nav/AuthModal/Signup.tsx
@@ -1,10 +1,18 @@
 /* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
-import React, { useState } from 'react';
+import React, { FormEvent, useEffect, useState } from 'react';
 
 // MUI
 import { makeStyles, useTheme } from '@mui/styles';
 import { Theme } from '@mui/material';
+
+// Redux
+import { useDispatch, useSelector } from 'react-redux';
+import { closeModal, toggleModalView } from '../../../actions/authModal';
+import { AppState } from '../../../../store';
+
+// Authentication hook
+import useAuth from '../../../hooks/useAuth';
 
 const useStyles = makeStyles((theme: Theme) => ({
 
@@ -17,17 +25,62 @@ interface LoginProps {
 }
 
 const Login = ({ handleModalViewToggle, view, classes }: LoginProps): JSX.Element => {
+  // Authentication functions
+  const { signup } = useAuth();
+  const dispatch = useDispatch();
+
+  // Redux state
+  const userState = useSelector((state: AppState) => state.userState);
+
   // Local state
   const [email, setEmail] = useState<string>('');
   const [password, setPassword] = useState<string>('');
   const [confirmPassword, setConfirmPassword] = useState<string>('');
+
+  // Styling
   const theme = useTheme();
+
+  const onSubmit = async (event: FormEvent) => {
+    event.preventDefault();
+    if (!email || !password) return;
+    try {
+      signup(email, password);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
-    <form className={classes.formContainer}>
+    <form className={classes.formContainer} onSubmit={onSubmit}>
       <p className={`${classes.headerText} no_margin`}>Create an Account</p>
-      <input type="email" placeholder="email" className={classes.formInput} value={email} onChange={(event: React.ChangeEvent<HTMLInputElement>) => setEmail(event.target.value)} />
-      <input type="password" placeholder="password" className={classes.formInput} value={password} onChange={(event: React.ChangeEvent<HTMLInputElement>) => setPassword(event.target.value)} />
-      <input type="password" placeholder="confirm password" className={classes.formInput} value={confirmPassword} onChange={(event: React.ChangeEvent<HTMLInputElement>) => setConfirmPassword(event.target.value)} />
+      <input
+        type="email"
+        placeholder="email"
+        className={classes.formInput}
+        value={email}
+        onChange={(event: React.ChangeEvent<HTMLInputElement>) => setEmail(event.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="password"
+        className={classes.formInput}
+        value={password}
+        onChange={(event: React.ChangeEvent<HTMLInputElement>) => setPassword(event.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="confirm password"
+        className={classes.formInput}
+        value={confirmPassword}
+        onChange={(event: React.ChangeEvent<HTMLInputElement>) => (
+          setConfirmPassword(event.target.value)
+        )}
+      />
+      {/* Will change below to error container */}
+      <span className={classes.toggleViewContainer}>
+        {userState.error
+          && <p className={`${classes.toggleViewText} no_margin`}>{userState.error}</p>}
+      </span>
       <button type="submit" className={classes.submitButton}>Sign Up</button>
       <span className={classes.toggleViewContainer}>
         Already have an account?
