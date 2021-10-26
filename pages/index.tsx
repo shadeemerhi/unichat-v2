610170210fb69e6414645a3872631e14fbe3f419
Shadee Merhi <shadmerhi@gmail.com>
2021-10-25 23:56:30 -0600

    remove SignIn comp - keep this for test

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,11 @@
 import { useSelector } from 'react-redux';
-import useAuth from '../src/hooks/useAuth';
 import { AppState } from '../store';
-
-// Components
-import SignIn from './SignIn';
 
 const Home = () => {
   const userState = useSelector((state: AppState) => state.userState);
   return (
     <>
       {userState.user ? <p>{`Welcome to UniChat, ${userState.user.email}`}</p> : <p>Login or Create an Account</p>}
-      <SignIn />
     </>
   );
 };
