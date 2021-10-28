39cfd4326909afc58d74ca2b2561f3c1ca334058
Shadee Merhi <shadmerhi@gmail.com>
2021-10-27 23:40:47 -0600

    replace random user name with actual user name

--- src/components/Nav/AuthModal/CreateUserName.tsx
+++ src/components/Nav/AuthModal/CreateUserName.tsx
@@ -10,6 +10,7 @@ import { CircularProgress } from '@mui/material';
 import { useDispatch, useSelector } from 'react-redux';
 import { setUserLoading, setUserError } from '../../../actions/user';
 import { AppState } from '../../../../store';
+import { UserState } from '../../../reducers/user';
 
 interface CreateUserNameProps {
   classes: any
@@ -18,22 +19,11 @@ interface CreateUserNameProps {
 const UserName = ({ classes }: CreateUserNameProps): JSX.Element => {
   // State
   const dispatch = useDispatch();
-  const userState = useSelector((state: AppState) => state.userState);
+  const userState: UserState = useSelector((state: AppState) => state.userState);
 
   // Local state
   const [username, setUsername] = useState('');
-  const [randomUsername, setRandomUsername] = useState('');
 
-  useEffect(() => {
-    const numberDictionary = NumberDictionary.generate({ min: 100, max: 999 });
-    const characterName: string = uniqueNamesGenerator({
-      dictionaries: [adjectives, animals, numberDictionary],
-      length: 3,
-      separator: '',
-      style: 'capital',
-    });
-    setRandomUsername(characterName);
-  }, []);
   return (
     <form className={classes.formContainer}>
       <p className={classes.headerText}>
@@ -49,9 +39,9 @@ const UserName = ({ classes }: CreateUserNameProps): JSX.Element => {
         value={username}
         onChange={(event: React.ChangeEvent<HTMLInputElement>) => setUsername(event.target.value)}
       />
-      <button type="submit" className={classes.submitButton}>{userState.loading ? <CircularProgress size={20} color="inherit" /> : 'Submit'}</button>
+      <button type="submit" className={classes.submitButton}>{userState.loading ? <CircularProgress size={20} color="inherit" /> : 'Save'}</button>
       <br />
-      <p className="no_margin">{randomUsername}</p>
+      <p className="no_margin">{userState.user?.username}</p>
       <p className={`${classes.coloredText} no_margin small_font cursor`}>Continue with default</p>
     </form>
   );
