f7daf171335259d86a378a7af2d918ace5de1122
Shadee Merhi <shadmerhi@gmail.com>
2021-10-29 00:46:06 -0600

    add update username functionality

--- src/components/Nav/AuthModal/CreateUserName.tsx
+++ src/components/Nav/AuthModal/CreateUserName.tsx
@@ -1,16 +1,17 @@
-import React, { useState, useEffect } from 'react';
-import {
-  uniqueNamesGenerator, NumberDictionary, adjectives, animals,
-} from 'unique-names-generator';
+import React, { useState, useEffect, FormEvent } from 'react';
 
 // MUI
 import { CircularProgress } from '@mui/material';
 
+// Axios
+import axios, { AxiosResponse } from 'axios';
+
 // Redux
 import { useDispatch, useSelector } from 'react-redux';
-import { setUserLoading, setUserError } from '../../../actions/user';
+import { setUserLoading, setUserError, logUserIn } from '../../../actions/user';
 import { AppState } from '../../../../store';
 import { UserState } from '../../../reducers/user';
+import { User } from '../../../types/User';
 
 interface CreateUserNameProps {
   classes: any
@@ -23,6 +24,38 @@ const UserName = ({ classes }: CreateUserNameProps): JSX.Element => {
 
   // Local state
   const [username, setUsername] = useState('');
+
+  const handleSubmit = async (event: FormEvent) => {
+    event.preventDefault();
+    if (userState.error) {
+      dispatch(setUserError(''));
+    }
+    dispatch(setUserLoading(true));
+
+    try {
+      const existingUserResponse: AxiosResponse<User> = await axios.get(`/users/username/${username}`);
+
+      // Check if an existing user has username
+      if (existingUserResponse.data) {
+        dispatch(setUserLoading(false));
+        dispatch(setUserError('Username not available'));
+      }
+
+      const updatedUserResponse: AxiosResponse<User> = await axios.put(
+        `/users/username/${userState.user?.uid}`,
+        { username },
+      );
+
+      if (updatedUserResponse.data) {
+        dispatch(setUserLoading(false));
+        dispatch(logUserIn(updatedUserResponse.data));
+      }
+    } catch (error: any) {
+      dispatch(setUserError(error.message));
+      dispatch(setUserLoading(false));
+      console.log(error);
+    }
+  };
 
   return (
     <form className={classes.formContainer}>
@@ -39,7 +72,13 @@ const UserName = ({ classes }: CreateUserNameProps): JSX.Element => {
         value={username}
         onChange={(event: React.ChangeEvent<HTMLInputElement>) => setUsername(event.target.value)}
       />
-      <button type="submit" className={classes.submitButton}>{userState.loading ? <CircularProgress size={20} color="inherit" /> : 'Save'}</button>
+      <span className={classes.toggleViewContainer}>
+        {userState.error
+          && <p className={`${classes.toggleViewText} no_margin cursor`}>{userState.error}</p>}
+      </span>
+      <button type="submit" className={classes.submitButton} onClick={handleSubmit}>
+        {userState.loading ? <CircularProgress size={20} color="inherit" /> : 'Save'}
+      </button>
       <br />
       <p className="no_margin">{userState.user?.username}</p>
       <p className={`${classes.coloredText} no_margin small_font cursor`}>Continue with default</p>
